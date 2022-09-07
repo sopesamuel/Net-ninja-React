f054ba2f80743ea899982e37a72ac4afa696862b
sopesamuel <fasusimosope@gmail.com>
2022-09-07 11:13:26 +0100

    first commit

--- index.js
+++ index.js
@@ -12,20 +12,45 @@
 
 // console.log(newNinjas, ninjas)
 
-const ninjas = [
-    {name: 'chuks', age: 30},
-    {name: 'Sops', age: 22},
-    {name: 'Gm', age: 45}
-]
+// const ninjas = [
+//     {name: 'chuks', age: 30},
+//     {name: 'Sops', age: 22},
+//     {name: 'Gm', age: 45}
+// ]
 
-const newNinjas = ninjas.filter((ninja)=>{
-return ninja.name === 'Gm'
-})
+// const newNinjas = ninjas.filter((ninja)=>{
+// return ninja.name === 'Gm'
+// })
 
-console.log( newNinjas, ninjas)
+// console.log( newNinjas, ninjas)
 
-const names = ninjas.map((ninja)=>{
-    return ninja.name
-})
+// const names = ninjas.map((ninja)=>{
+//     return ninja.name
+// })
 
-console.log(names)
+// console.log(names)
+
+// const game = {name: 'chuks', age: 30}
+
+// const gamers = {...game, id: 5}
+
+// console.log(gamers)
+
+// const name = 'Sope'
+// const job = 'Software developer'
+
+// const Introduction = `Hi my name is ${name} and i am a ${job}`
+
+// console.log(Introduction)
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+// .then((res) => res.json())
+// .then((data) => console.log(data))
+
+// const todo = async () => {
+// const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+// const data =  await res.json()
+// console.log(data)
+// }
+
+// todo()
